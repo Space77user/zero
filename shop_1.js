eeeae819bc33284fd9466178ef1f1a732886691c
Space77user <rinatazimov62@gmail.com>
2025-05-23 23:23:41 +0600

    Add files via upload

---
+++ shop_1.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("burger1").addEventListener("click", function () {
+    document.querySelector("header").classList.toggle("open");
+  });
+});
+
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// console.log(c);
